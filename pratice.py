b6768977c1e20a693b09fb440a517886ed288c3f
ssammu <hjth0911@ajou.ac.kr>
2020-08-05 01:34:12 +0900

    Study basic things

---
+++ pratice.py
@@ -0,0 +1,19 @@
+# print(5/3)
+# print(5//3)  # // 은 소숫점 계산을 하지 않고 몫만 출력하게 해준다.
+# print(abs(-5))
+# print(pow(4,2))
+# print(round(5.8))
+
+# from math import *
+# print(sqrt(16))
+
+from random import *
+
+# print(random()) # 0.0 ~ 1.0 미만의 임의의 값 생성
+# print(random()* 10) # 0.0 ~ 10.0 미만의 임의의 값 생성 
+# print(int(random()*10)) # 0 ~ 10 미만의 임의의 값 생성 
+# print(int(random()* 10) +1) #1 ~ 10 이하의 임의의 값 생성 
+# print(randrange(1, 10)) # 1 ~ 10 미만의 임의의 값 생성
+# print(randint(1, 10)) # 1 ~ 10이하의 임의의 값 생성 
+
+print("오프라인 스터디 모임 날짜는 매월",randint(4, 28),"일로 선정되었습니다.")
